7fd29397f5cec0e19f195953630a53f976ab778d
Theo Butler <theodusbutler@gmail.com>
2024-01-15 11:49:39 -0500

    simulator: generate candidates

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,19 +1,22 @@
-use candidate_selection::Normalized;
+use candidate_selection::{criteria::performance::Performance, Normalized};
+use indexer_selection::Candidate;
+use rand::{rngs::SmallRng, Rng, SeedableRng};
 use std::io::stdin;
 use thegraph::types::Address;
 
 pub struct IndexerCharacteristics {
     pub address: Address,
     pub fee: f64,
-    pub avg_seconds_behind: u16,
-    pub avg_latency_ms: u32,
+    pub seconds_behind: u16,
+    pub latency_ms: u32,
     pub success_rate: Normalized,
-    pub slashable_stake_grt: u128,
-    pub allocation_grt: u128,
+    pub slashable_usd: u64,
+    pub zero_allocation: bool,
 }
 
 fn main() {
-    let header = "address,fee_grt,avg_seconds_behind,avg_latency_ms,success_rate,slashable_stake_grt,allocation_grt";
+    let header =
+        "address,fee_grt,seconds_behind,latency_ms,success_rate,slashable_stake_grt,allocation_grt";
     let characteristics: Vec<IndexerCharacteristics> = stdin()
         .lines()
         .filter_map(|line| {
@@ -25,16 +28,38 @@ fn main() {
             Some(IndexerCharacteristics {
                 address: fields[0].parse().expect("address"),
                 fee: fields[1].parse().expect("fee"),
-                avg_seconds_behind: fields[2].parse().expect("avg_seconds_behind"),
-                avg_latency_ms: fields[3].parse().expect("avg_latency_ms"),
+                seconds_behind: fields[2].parse().expect("seconds_behind"),
+                latency_ms: fields[3].parse().expect("latency_ms"),
                 success_rate: fields[4]
                     .parse::<f64>()
                     .ok()
                     .and_then(Normalized::new)
                     .expect("success_rate"),
-                slashable_stake_grt: fields[5].parse().expect("slashable_stake_grt"),
-                allocation_grt: fields[6].parse().expect("allocation_grt"),
+                slashable_usd: fields[5].parse().expect("slashable_usd"),
+                zero_allocation: fields[6].parse().expect("zero_allocation"),
             })
+        })
+        .collect();
+
+    let mut rng = SmallRng::from_entropy();
+    let budget = 20e-6;
+    let candidates: Vec<Candidate> = characteristics
+        .into_iter()
+        .map(|c| {
+            let mut performance = Performance::new();
+            for _ in 0..1000 {
+                performance.feedback(rng.gen_bool(c.success_rate.as_f64()), c.latency_ms);
+            }
+            Candidate {
+                indexer: c.address,
+                deployment: [0; 32].into(),
+                fee: Normalized::new(c.fee / budget).expect("invalid fee or budget"),
+                subgraph_versions_behind: 0,
+                seconds_behind: c.seconds_behind,
+                slashable_usd: c.slashable_usd,
+                zero_allocation: c.zero_allocation,
+                performance: Box::leak(Box::new(performance)),
+            }
         })
         .collect();
 
